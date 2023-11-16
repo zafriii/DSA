e5849979ad5bf5ea3dbe9b4c7cc81aeac9e9c246
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2023-11-16 21:55:38 +0600

    Add files via upload

---
+++ sqrtdiv.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int sum = 0,count = 0;
+    for(int i =1;i*i<=n;++i){
+       if(n%i==0){
+
+       cout<<i<<n/i<<endl;
+        count+= 1;
+        sum+= i;
+
+       if(n/i!= i){
+         count+= 1;
+         sum+= n/i;
+       }
+      }
+    }
+
+    cout<<count<<sum<<endl;
+}
